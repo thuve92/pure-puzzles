0d859e23db1caed3ce25b88ff27c61104bdef125
thuve92 <72792411+thuve92@users.noreply.github.com>
2021-03-08 12:58:37 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pure_puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
